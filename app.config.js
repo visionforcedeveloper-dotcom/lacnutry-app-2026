93e36c8b89fd7a3167900c656f590ef1877a5efe
LacNutry Dev <dev@lacnutry.com>
2026-01-22 08:45:52 -0300

    chore: bump version to 3.60.1 (92)

--- app.config.js
+++ app.config.js
@@ -9,7 +9,7 @@ module.exports = {
   expo: {
     name: IS_DEV ? 'LacNutry (DEV)' : 'LacNutry',
     slug: 'lacnutry-app',
-    version: IS_DEV ? '3.9.0' : '3.60.0',
+    version: IS_DEV ? '3.9.0' : '3.60.1',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
     scheme: 'rork-app',
@@ -30,7 +30,7 @@ module.exports = {
       },
     },
     android: {
-      versionCode: 91,
+      versionCode: 92,
       adaptiveIcon: {
         foregroundImage: './assets/images/adaptive-icon.png',
         backgroundColor: '#ffffff',
